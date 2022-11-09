58a74f9bfd8bd36491ef975fd2456902a9982562
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-11-09 21:09:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-
 getData()
+charted()
+
 async function getData() {
     const response = await fetch('ZonAnn.Ts+dSST.csv');
     const data = await response.text();
     // console.log(data);
 
-    const table = data.split('\n').slice(1);
+    const table = data.split('\n').splice(1);
     table.forEach(row => {
-        const colums = row.slice(',');
+        const colums = row.split(',');
         const year = colums[0];
+        xlabels.push(year)
         const temp = colums[1];
+        ytemps.push(temp)
         const boo = colums[2]
-        console.log(year,temp,boo);
+        yboo.push(boo)
+        console.log(year, temp, boo);
     });
-    
-}
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+}
+const xlabels = []
+const ytemps = []
+const yboo= []
+
+async function charted() {
+    await getData()
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xlabels,
+            datasets: [{
+                label: 'GISS Surface Temperature Analysis',
+                data: ytemps,
+                data: yboo,
+                backgroundColor: 
+                    'rgba(255, 99, 132, 0.2)',
+                borderColor: 
+                    'rgba(255, 99, 132, 1)',
+                   
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
