f422d0c7351cdb472ca01d456f81ebe4546cf50c
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-11-09 20:26:29 +0100

    added chart.js library

--- app.js
+++ app.js
@@ -5,12 +5,49 @@ async function getData() {
     const data = await response.text();
     // console.log(data);
 
-    const rows = data.split('\n').slice(1);
-    rows.forEach(elt => {
-        const row = elt.slice(',');
-        const year = row[0];
-        const temp = row[1];
-        console.log(year,temp);
+    const table = data.split('\n').slice(1);
+    table.forEach(row => {
+        const colums = row.slice(',');
+        const year = colums[0];
+        const temp = colums[1];
+        const boo = colums[2]
+        console.log(year,temp,boo);
     });
     
 }
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
